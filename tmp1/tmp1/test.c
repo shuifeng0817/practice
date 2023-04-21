f59a548b1b2bd10c527ebb7c5dd19f6d32dbf768
shuifeng0817 <582231125@qq.com>
2023-04-21 09:13:45 +0800

    2023年4月21

--- tmp1/tmp1/test.c
+++ tmp1/tmp1/test.c
@@ -2,6 +2,22 @@
 #include<stdio.h>
 int main()
 {
-	printf("hello");
+	char ch;
+	long n,nx = 0;
+	long m = 0;
+	while ((ch = getchar()) != '\n')
+	{
+		if (ch >= '0' && ch <= '9')
+			m = m * 10 + ch - '0';
+		else
+			putchar(ch);
+	}
+	n = m;
+	while (m != 0)
+	{
+		nx = nx * 10 + m % 10;
+		m = m / 10;
+	}
+	printf("\n%ld reverse number id %ld\n", n, nx);
 	return 0;
 }
